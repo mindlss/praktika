d02c115bdcf73a0dad5603c29af8f864704cf4fb
vard05 <a.a.arkhipov05@gmail.com>
2024-06-30 00:18:28 +0300

    Маленькие изменения

--- backend/parser/index.js
+++ backend/parser/index.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
 console.log('Parser is active');
 
@@ -7,19 +8,24 @@ async function getHtml(url) {
     const response = await fetch(url, {
         headers: {
             'User-Agent':
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Viewport-Width': '1920',
         },
     });
     const html = await response.text();
     return html;
 }
 
-function parse(html) {
-    const $ = cheerio.load(html);
+function parseStats($) {
+    const stats = {}
+    console.log($('[data-qa="bloko-header-3"]:first').text())
+}
 
+function parseVacancies($) {
     const vacancies = [];
 
-    $('.vacancy-search-item__card').each((i, elem) => {
+    $('[data-qa~="vacancy-serp__vacancy"]').each((i, elem) => {
         const isTrustedEmployer =
             $(elem).find('[data-qa="trusted-employer-link"]').length > 0;
         const isAccreditedEmployer =
@@ -61,11 +67,23 @@ function parse(html) {
     return vacancies;
 }
 
+function parse(html) {
+    const $ = cheerio.load(html);
+    parseStats($)
+
+    const vacancies = parseVacancies($);
+    return vacancies;
+}
+
 async function main() {
     const url =
-        'https://hh.ru/search/vacancy?search_field=name&search_field=company_name&search_field=description&text=Backend&enable_snippets=true';
+        'https://hh.ru/search/vacancy?search_field=name&search_field=company_name&search_field=description&text=Backend&enable_snippets=true&page=0';
     const html = await getHtml(url);
-    console.log(parse(html));
+    // save html to file
+    fs.writeFileSync('index.html', html);
+    const vacancies = parse(html);
+    console.log(vacancies);
+    console.log(vacancies.length);
 }
 
 main();
